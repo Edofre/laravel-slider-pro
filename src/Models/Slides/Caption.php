4b9db0c5ba5871bb18fe91663157c6c9b9f52147
Edo Freriks <edofre@gmail.com>
2016-12-17 23:36:11 +0100

    Added caption model

---
+++ src/Models/Slides/Caption.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Edofre\SliderPro\Models\Slides;
+
+/**
+ * Class Caption
+ * @package Edofre\SliderPro\Models\Slides
+ */
+class Caption extends \Edofre\SliderPro\Models\SliderProModel
+{
+    /** Required class for the captions */
+    const CAPTION_CLASS = 'sp-caption';
+
+    /** @var  string The tag (a, div, span etc) that will be rendered */
+    public $tag;
+    /** @var  string The contents of the tag */
+    public $content;
+    /** @var  array The HTML options for the layer */
+    public $htmlOptions;
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        if (isset($this->htmlOptions['class'])) {
+            $this->htmlOptions['class'] = self::CAPTION_CLASS . ' ' . $this->htmlOptions['class'];
+        } else {
+            $this->htmlOptions['class'] = self::CAPTION_CLASS;
+        }
+
+
+        //return \yii\bootstrap\Html::tag($this->tag, $this->content, $this->htmlOptions);
+    }
+}
