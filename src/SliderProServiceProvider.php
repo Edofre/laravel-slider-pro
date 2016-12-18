e97337fe634d4e2e5eeda54157b95aa655bc0d47
Edo Freriks <edofre@gmail.com>
2016-12-18 14:14:45 +0100

    Added service provider

---
+++ src/SliderProServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Edofre\SliderPro;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class SliderProServiceProvider
+ * @package Edofre\SliderPro
+ */
+class SliderProServiceProvider extends ServiceProvider
+{
+    /** Identifier for the service */
+    const IDENTIFIER = 'laravel-slider-pro';
+
+    /**
+     * Register bindings in the container.
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(self::IDENTIFIER, function ($app) {
+            return $app->make(SliderPro::class);
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     * @return void
+     */
+    public function boot()
+    {
+        // specify from where we want to load the views
+        $this->loadViewsFrom(__DIR__ . '/views/', 'slider-pro');
+
+        // publish all the required files to generate the slider
+        $this->publishes([
+            // fullcalendar library
+            __DIR__ . '/../../../bower-asset/slider-pro/dist/css/slider-pro.css'     => public_path('css/slider-pro.css'),
+            __DIR__ . '/../../../bower-asset/slider-pro/dist/js/jquery.sliderPro.js' => public_path('js/slider-pro.js'),
+        ], 'slider-pro');
+    }
+
+    /**
+     * @return array
+     */
+    public function provides()
+    {
+        return [self::IDENTIFIER];
+    }
+}
