0ddf018f0fc4f3f523e723461fcf1df5bf365735
Edo Freriks <edofre@gmail.com>
2016-12-18 14:15:44 +0100

    Added scripts to view

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -1,3 +1,6 @@
+<link href="/css/slider-pro.css" rel="stylesheet">
+<script type="text/javascript" src="/js/slider-pro.js"></script>
+
 <script type="text/javascript">
     jQuery(document).ready(function () {
         jQuery('#{{ $id }}').sliderPro({!! $options !!});
