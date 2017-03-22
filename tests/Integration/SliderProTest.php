16ce144e25c94d9c46fd2ce5de9b5c3e724a27b7
Edo Freriks <edofre@gmail.com>
2017-03-22 22:35:48 +0100

    Add test without generating divs

--- tests/Integration/SliderProTest.php
+++ tests/Integration/SliderProTest.php
@@ -43,4 +43,37 @@ class SliderProTest extends TestCase
     });
 </script>", $slider->generate());
     }
+
+    /** @test */
+    public function generate_slider_without_divs()
+    {
+        $slider = new SliderPro();
+        $slider->setId('my-slider');
+        $slider->setOptions([
+            'sliderOptions' => [
+                'width'  => 960,
+                'height' => 500,
+                'arrows' => true,
+                'init'   => new \Edofre\SliderPro\JsExpression("
+                        function() {
+                            console.log('slider is initialized');
+                        }
+                    "),
+            ],
+        ]);
+
+        // This looks terrible, I'm sorry...
+        $this->assertEquals("<link href=\"/css/slider-pro.css\" rel=\"stylesheet\">
+<script type=\"text/javascript\" src=\"/js/slider-pro.js\"></script>
+
+<script type=\"text/javascript\">
+    jQuery(document).ready(function () {
+        jQuery('#my-slider').sliderPro({\"sliderOptions\":{\"width\":960,\"height\":500,\"arrows\":true,\"init\":
+                        function() {
+                            console.log('slider is initialized');
+                        }
+                    }});
+    });
+</script>", $slider->generate(false));
+    }
 }
