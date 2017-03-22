43c2babc7053799087f97563807aaf2ba83c8244
Edo Freriks <edofre@gmail.com>
2017-03-22 22:35:31 +0100

    SliderProTest

---
+++ tests/Integration/SliderProTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Edofre\SliderPro\Test\Integration;
+
+use Edofre\SliderPro\SliderPro;
+
+/**
+ * Class SliderProTest
+ * @package Edofre\SliderPro\Test\Integration
+ */
+class SliderProTest extends TestCase
+{
+
+    /** @test */
+    public function generate_slider()
+    {
+        $slider = new SliderPro();
+        $slider->setId('my-slider');
+        $slider->setOptions([
+            'sliderOptions' => [
+                'width'  => 960,
+                'height' => 500,
+                'arrows' => true,
+                'init'   => new \Edofre\SliderPro\JsExpression("
+                        function() {
+                            console.log('slider is initialized');
+                        }
+                    "),
+            ],
+        ]);
+
+        // This looks terrible, I'm sorry...
+        $this->assertEquals("<div id='my-slider'></div><link href=\"/css/slider-pro.css\" rel=\"stylesheet\">
+<script type=\"text/javascript\" src=\"/js/slider-pro.js\"></script>
+
+<script type=\"text/javascript\">
+    jQuery(document).ready(function () {
+        jQuery('#my-slider').sliderPro({\"sliderOptions\":{\"width\":960,\"height\":500,\"arrows\":true,\"init\":
+                        function() {
+                            console.log('slider is initialized');
+                        }
+                    }});
+    });
+</script>", $slider->generate());
+    }
+}
